b54f489622d8428818198efc64422e831d45301e
xuhaitaoao—_glb <xuhaitao_glb@kgc.com>
2020-12-15 14:39:08 +0800

    Initial commit

---
+++ kmall-user/src/main/java/com/kgc/tcmp077/kmalluser/KmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.tcmp077.kmalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmallUserApplication.class, args);
+	}
+
+}
