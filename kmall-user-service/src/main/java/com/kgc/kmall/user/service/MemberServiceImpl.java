7e9da1fd3b7a4bb6e9dcb9578f75cba66a07e951
xuhaitaoao—_glb <xuhaitao_glb@kgc.com>
2020-12-15 17:22:34 +0800

    kmall-user-web实现类

--- kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
+++ kmall-user-service/src/main/java/com/kgc/kmall/user/service/MemberServiceImpl.java
@@ -3,10 +3,14 @@ package com.kgc.kmall.user.service;
 import com.kgc.kmall.bean.Member;
 import com.kgc.kmall.service.MemberService;
 import com.kgc.kmall.user.mapper.MemberMapper;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
+
 
 import javax.annotation.Resource;
 import java.util.List;
+
+@Component
 @Service
 public class MemberServiceImpl implements MemberService {
     @Resource
