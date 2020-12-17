ee89a8a8184f663606688778a4a0be784bd08d85
xuhaitaoao—_glb <xuhaitao_glb@kgc.com>
2020-12-17 14:21:37 +0800

    商品spu管理-显示spu列表 逆向工程文件

---
+++ kmallapi/src/main/java/com/kgc/kmall/bean/pmsProductSaleAttrValue.java
@@ -0,0 +1,43 @@
+package com.kgc.kmall.bean;
+
+public class pmsProductSaleAttrValue {
+    private Long id;
+
+    private Long productId;
+
+    private Long saleAttrId;
+
+    private String saleAttrValueName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getSaleAttrId() {
+        return saleAttrId;
+    }
+
+    public void setSaleAttrId(Long saleAttrId) {
+        this.saleAttrId = saleAttrId;
+    }
+
+    public String getSaleAttrValueName() {
+        return saleAttrValueName;
+    }
+
+    public void setSaleAttrValueName(String saleAttrValueName) {
+        this.saleAttrValueName = saleAttrValueName == null ? null : saleAttrValueName.trim();
+    }
+}
